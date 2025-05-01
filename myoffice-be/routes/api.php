6ee832acfb11103fd34b822e914e08f97c9c43dd
Frynnn-69 <firyanfatihh@gmail.com>
2025-05-01 11:20:15 +0700

    chore: Add Laravel Sanctum dependency and API routes

---
+++ myoffice-be/routes/api.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Http\Controllers\API\BookingTransactionController;
+use App\Http\Controllers\API\CityController;
+use App\Http\Controllers\API\OfficeSpaceController;
+use App\Http\Controllers\API\BookingTransactionControllerr;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+    Route::get('/city/{city:slug}', [CityController::class, 'show']);
+    Route::apiResource('/cities', CityController::class); //alternatif route untuk endpoint cities (api resource udah handle CRUD)
+
+    Route::get('/office/{officeSpace:slug', [OfficeSpaceController::class, 'show']);
+    Route::apiResource('/office', OfficeSpaceController::class); 
+
+    Route::post('/booking-transcation', [BookingTransactionController::class, 'store']); 
+    Route::post('/check-booking', [BookingTransactionController::class, 'booking_details']);
