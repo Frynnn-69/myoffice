4bbee8880d5bbc88fd78c91ee2361f97168ea8aa
Frynnn-69 <firyanfatihh@gmail.com>
2025-05-14 21:05:02 +0700

    feat: Add booking transaction creation endpoint and request validation

--- myoffice-be/app/Http/Controllers/API/BookingTransactionController.php
+++ myoffice-be/app/Http/Controllers/API/BookingTransactionController.php
@@ -3,9 +3,36 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreBookingTransactionRequest;
+use App\Models\BookingTransaction;
+use App\Models\OfficeSpace;
 use Illuminate\Http\Request;
 
 class BookingTransactionController extends Controller
 {
-    //
+    public function store(StoreBookingTransactionRequest $request)
+    {
+        // Validate the request
+        $validatedData = $request->validated();
+
+        $officeSpace = OfficeSpace::find($validatedData['office_space_id']); //cek apakah office space ada
+
+        //cek apakah office space sudah dibooking
+        $validatedData['is_paid'] = false;
+        $validatedData['booking_trx_id'] = BookingTransaction::generateUniqeTrxId();
+        $validatedData['duration'] = $officeSpace->duration;
+
+        //cek kapan booking dimulai dan berakhir
+        $validatedData['ended_date'] = (new \DateTime($validatedData['started_date']))->modify("+{$officeSpace->duration} days")->format('Y-m-d');
+
+        $bookingTransaction = BookingTransaction::create($validatedData);
+
+        //kirim notif via sms dan whatsapp with twilio
+        // $this->sendNotification($bookingTransaction);
+        // return response()->json([
+        //     'message' => 'Booking transaction created successfully',
+        //     'data' => $bookingTransaction,
+        // ], 201);
+        
+    }
 }
--- myoffice-be/routes/api.php
+++ myoffice-be/routes/api.php
@@ -18,7 +18,6 @@ Route::middleware('api_key')->group(function () { // middleware untuk memeriksa 
     Route::get('/office/{officeSpace:slug}', [OfficeSpaceController::class, 'show']);
     Route::apiResource('/offices', OfficeSpaceController::class); 
 
-    Route::post('/booking-transcation', [BookingTransactionController::class, 'store']); 
+    Route::post('/booking-transaction', [BookingTransactionController::class, 'store']);
     Route::post('/check-booking', [BookingTransactionController::class, 'booking_details']);
-
 });
